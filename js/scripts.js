f98cd0a86ed1dfcfbff7e494f817d9895aa68757
twwli <hello@theworldwelivein.co.uk>
2025-03-17 07:50:39 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,28 +24,5 @@ jQuery(document).ready(function($) {
       });
     $('body').toggleClass('no-overflow');
   });
-
-  /* VANILLA JS */
-
-  document.querySelector('.toggle-menu').addEventListener('click', function () {
-    this.classList.toggle('active');
-    
-    const wrapper = document.getElementById('mobile-nav-wrapper');
-    const nav = document.getElementById('mobile-nav');
-    const body = document.body;
-
-    wrapper.classList.toggle('open');
-    nav.classList.toggle('open');
-    body.classList.toggle('no-overflow');
-
-    const navItems = nav.querySelectorAll('li');
-    
-    navItems.forEach((li, i) => {
-        setTimeout(() => {
-            li.classList.toggle('anim');
-        }, i * 100);  // Délai progressif pour chaque élément
-    });
-});
-
   
 }); /* end of as page load scripts */
